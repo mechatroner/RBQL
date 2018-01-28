776b9327466dd18edb6c55a89fe3c24b4cc58861
mechatroner <mechatroner@yandex.ru>
2018-01-28 11:57:04 -0500

    add rbql.js

---
+++ rbql.js
@@ -0,0 +1,16 @@
+
+const version = '0.1.0';
+
+
+const GROUP_BY = 'GROUP BY';
+const UPDATE = 'UPDATE';
+const SELECT = 'SELECT';
+const JOIN = 'JOIN';
+const INNER_JOIN = 'INNER JOIN';
+const LEFT_JOIN = 'LEFT JOIN';
+const STRICT_LEFT_JOIN = 'STRICT LEFT JOIN';
+const ORDER_BY = 'ORDER BY';
+const WHERE = 'WHERE';
+const LIMIT = 'LIMIT';
+
+const rbql_home_dir = __dirname;
