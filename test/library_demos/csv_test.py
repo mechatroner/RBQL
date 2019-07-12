c66b45587a8c753871f1b4aa5160f1cf1a7b33e7
mechatroner <mechatroner@yandex.ru>
2019-07-12 01:06:55 -0400

    better demo code

--- test/library_demos/csv_test.py
+++ test/library_demos/csv_test.py
@@ -4,8 +4,6 @@ from rbql import rbql_csv
 user_query = 'SELECT a1, int(a2) % 1000 WHERE a3 != "USA" LIMIT 5'
 error_info, warnings = rbql_csv.csv_run(user_query, 'input.csv', ',', 'quoted', 'output.csv', ',', 'quoted', 'utf-8')
 if error_info is None:
-    print('result table:')
-    result_data = open('output.csv').read()
-    print(result_data)
+    print(open('output.csv').read())
 else:
     print('Error: {}: {}'.format(error_info['type'], error_info['message']))
--- test/library_demos/table_test.py
+++ test/library_demos/table_test.py
@@ -1,11 +1,18 @@
 import rbql
 from rbql import rbql_csv
 
-user_query = 'SELECT a1, int(a2) % 1000 WHERE a3 != "USA" LIMIT 5'
-error_info, warnings = rbql_csv.csv_run(user_query, 'input.csv', ',', 'quoted', 'output.csv', ',', 'quoted', 'utf-8')
+input_table = [
+    ['Roosevelt',1858,'USA'],
+    ['Napoleon',1769,'France'],
+    ['Dmitri Mendeleev',1834,'Russia'],
+    ['Jane Austen',1775,'England'],
+    ['Hayao Miyazaki',1941,'Japan'],
+]
+user_query = 'SELECT a1, int(a2) % 1000 WHERE a3 != "USA" LIMIT 3'
+output_table = []
+error_info, warnings = rbql.table_run(user_query, input_table, output_table)
 if error_info is None:
-    print('result table:')
-    result_data = open('output.csv').read()
-    print(result_data)
+    for record in output_table:
+        print(','.join([str(v) for v in record]))
 else:
     print('Error: {}: {}'.format(error_info['type'], error_info['message']))
