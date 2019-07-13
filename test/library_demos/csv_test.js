9ab47ba7ec30603e2e865747e5e63fbc30e48998
mechatroner <mechatroner@yandex.ru>
2019-07-13 12:54:36 -0400

    better demo

---
+++ test/library_demos/csv_test.js
@@ -0,0 +1,12 @@
+const rbql_csv = require('rbql_csv.js');
+
+let user_query = 'SELECT a1, parseInt(a2) % 1000 WHERE a3 != "USA" LIMIT 5';
+let error_handler = function(error_type, error_msg) {
+    console.log('Error: ' + error_type + ': ' + error_msg);
+}
+let success_handler = function(warnings) {
+    if (warnings.length)
+        console.log('warnings: ' + JSON.stringify(warnings));
+    console.log('output table: output.csv');
+}
+rbql_csv.csv_run(user_query, 'input.csv', ',', 'quoted', 'output.csv', ',', 'quoted', 'utf-8', success_handler, error_handler);
