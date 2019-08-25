413f5ae5db917550da2f138082508c3c4aa023be
mechatroner <mechatroner@yandex.ru>
2019-08-25 00:30:19 -0400

    update version

--- rbql-js/rbql.js
+++ rbql-js/rbql.js
@@ -795,7 +795,7 @@ module.exports.rb_transform = rb_transform;
 // TODO replace prototypes with classes: this improves readability
 
 
-const version = '0.8.0';
+const version = '0.9.0';
 
 const GROUP_BY = 'GROUP BY';
 const UPDATE = 'UPDATE';
