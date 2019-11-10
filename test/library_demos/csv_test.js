032a2b55890dcfc12172c6ff74bfb2f617be0e62
mechatroner <mechatroner@yandex.ru>
2019-11-10 11:19:27 -0500

    fix test

--- test/library_demos/csv_test.js
+++ test/library_demos/csv_test.js
@@ -1,12 +1,15 @@
-const rbql = require('rbql');
+const rbql_csv = require('rbql_csv');
 
 let user_query = 'SELECT a1, parseInt(a2) % 1000 WHERE a3 != "USA" LIMIT 5';
-let error_handler = function(error_type, error_msg) {
-    console.log('Error: ' + error_type + ': ' + error_msg);
+
+let error_handler = function(exception) {
+    console.log('Error: ' + JSON.stringify(exception));
 }
+
 let success_handler = function(warnings) {
     if (warnings.length)
         console.log('warnings: ' + JSON.stringify(warnings));
     console.log('output table: output.csv');
 }
-rbql.csv_run(user_query, 'input.csv', ',', 'quoted', 'output.csv', ',', 'quoted', 'utf-8', success_handler, error_handler);
+
+rbql_csv.csv_run(user_query, 'input.csv', ',', 'quoted', 'output.csv', ',', 'quoted', 'utf-8').then(success_handler).catch(error_handler);
