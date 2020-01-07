ffdedba966b04dff927829a17f3679391bacf77a
mechatroner <mechatroner@yandex.ru>
2020-01-06 23:41:43 -0500

    cosmetic

--- test/library_demos/csv_test.js
+++ test/library_demos/csv_test.js
@@ -1,17 +1,12 @@
 const rbql_csv = require('rbql_csv');
-
 let user_query = 'SELECT a1, parseInt(a2) % 1000 WHERE a3 != "USA" LIMIT 5';
-
 let error_handler = function(exception) {
     console.log('Error: ' + String(exception));
 }
-
 let warnings = [];
-
 let success_handler = function() {
     if (warnings.length)
         console.log('warnings: ' + JSON.stringify(warnings));
     console.log('output table: output.csv');
 }
-
 rbql_csv.query_csv(user_query, 'input.csv', ',', 'quoted', 'output.csv', ',', 'quoted', 'utf-8', warnings).then(success_handler).catch(error_handler);
--- test/library_demos/table_test.js
+++ test/library_demos/table_test.js
@@ -1,5 +1,4 @@
 const rbql = require('rbql')
-
 let input_table = [
     ['Roosevelt',1858,'USA'],
     ['Napoleon',1769,'France'],
@@ -7,18 +6,14 @@ let input_table = [
     ['Jane Austen',1775,'England'],
     ['Hayao Miyazaki',1941,'Japan'],
 ];
-
 let user_query = 'SELECT a1, a2 % 1000 WHERE a3 != "USA" LIMIT 3';
 let output_table = [];
 let warnings = [];
-
 let error_handler = function(exception) {
     console.log('Error: ' + String(exception));
 }
-
 let success_handler = function() {
     console.log('warnings: ' + JSON.stringify(warnings));
     console.log('output table: ' + JSON.stringify(output_table));
 }
-
 rbql.query_table(user_query, input_table, output_table, warnings).then(success_handler).catch(error_handler);
