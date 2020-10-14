09f25ff76bdf28c3a4cbf270c10c0759cc8806bb
mechatroner <mechatroner@yandex.ru>
2020-10-14 01:19:39 -0400

    switch to readline

--- rbql/rbql_json.py
+++ rbql/rbql_json.py
@@ -17,7 +17,9 @@ from . import rbql_csv
 from . import csv_utils
 
 
-# FIXME consider using python built-in readline function instead
+# Json input doesn't need to support json join because star * operator wouldn't make any sense in this case, 
+# the only way to concatenate JSONs is to put both of them inside a 2-element array, like this [json_1, json_2], but this is not practical
+# Another problem is with reading JOIN table - we would have to adjust variable -> index mapping code and instead of index use attribute, because JSON doesn't have column indices unlike CSV.
 
 
 class JSONLinesRecordIterator(rbql_engine.RBQLInputIterator):
@@ -28,13 +30,9 @@ class JSONLinesRecordIterator(rbql_engine.RBQLInputIterator):
         self.variable_prefix = variable_prefix
         self.comment_prefix = comment_prefix if (comment_prefix is not None and len(comment_prefix)) else None
 
-        self.buffer = ''
-        self.detected_line_separator = '\n'
         self.exhausted = False
         self.NR = 0 # Record number
         self.NL = 0 # Line number (NL != NR when the JSON Lines file has comments)
-        self.chunk_size = chunk_size
-        self.fields_info = dict()
 
 
     def get_variables_map(self, query_text):
@@ -43,58 +41,10 @@ class JSONLinesRecordIterator(rbql_engine.RBQLInputIterator):
         return variable_map
 
 
-    def _get_row_from_buffer(self):
-        str_before, separator, str_after = csv_utils.extract_line_from_data(self.buffer)
-        if separator is None:
-            return None
-        if separator == '\r' and str_after == '':
-            one_more = self.stream.read(1)
-            if one_more == '\n':
-                separator = '\r\n'
-            else:
-                str_after = one_more
-        self.detected_line_separator = separator
-        self.buffer = str_after
-        return str_before
-
-
-    def _read_until_found(self):
-        if self.exhausted:
-            return
-        chunks = []
-        while True:
-            chunk = self.stream.read(self.chunk_size)
-            if not chunk:
-                self.exhausted = True
-                break
-            chunks.append(chunk)
-            if csv_utils.newline_rgx.search(chunk) is not None:
-                break
-        self.buffer += ''.join(chunks)
-
-
-    def get_row_simple(self):
-        try:
-            row = self._get_row_from_buffer()
-            if row is None:
-                self._read_until_found()
-                row = self._get_row_from_buffer()
-                if row is None:
-                    assert self.exhausted
-                    if not len(self.buffer):
-                        return None
-                    row = self.buffer
-                    self.buffer = ''
-            self.NL += 1
-            return row
-        except UnicodeDecodeError:
-            raise rbql_engine.RbqlIOHandlingError('Unable to decode input data as UTF-8')
-
-    
     def get_record(self):
         while True:
-            line = self.get_row_simple()
-            if line is None:
+            line = self.stream.readline()
+            if not line:
                 return None
             if self.comment_prefix is None or not line.startswith(self.comment_prefix):
                 break
